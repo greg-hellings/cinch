5cbb8c022886f5039da9cd19d579f3a6228ad947
Gregory Hellings <greg.hellings@gmail.com>
2017-10-04 22:45:22 -0500

    Testing

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='cinch',
-    version='0.8.4.1',
+    version='0.8.4.2',
     description='Cinch continuous integration setup',
     long_description=description,
     url='https://github.com/RedHatQE/cinch',
@@ -29,7 +29,7 @@ setup(
     packages=find_packages(exclude=('library', 'bin')),
     include_package_data=True,
     install_requires=[
-        'ansible>=2.3.2,<2.4',
+        'ansible>=2.3.2,!2.4.0',
         'plumbum>=1.6.0',
         'linchpin>=1.0.4'
     ],
